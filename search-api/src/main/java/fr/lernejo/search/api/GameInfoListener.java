cbd7221faaed9d359a3cc1a9e0895dcaf1765849
LE huu phuc <huu-phuc.le@epita.fr>
2022-12-16 15:37:14 +0100

    [PROJECT] : Partie 5 (OP) - add new solution of AMQP => ES

--- search-api/src/main/java/fr/lernejo/search/api/GameInfoListener.java
+++ search-api/src/main/java/fr/lernejo/search/api/GameInfoListener.java
@@ -3,6 +3,7 @@ package fr.lernejo.search.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.elasticsearch.ElasticsearchException;
+import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.RequestOptions;
@@ -23,7 +24,6 @@ import static fr.lernejo.search.api.AmqpConfiguration.GAME_INFO_QUEUE;
 public class GameInfoListener {
 
     private final RestHighLevelClient client;
-    private final ObjectMapper mapper = new ObjectMapper().findAndRegisterModules();
 
     public GameInfoListener(RestHighLevelClient rest) {
         this.client = rest;
@@ -32,12 +32,33 @@ public class GameInfoListener {
     @RabbitListener(queues = {GAME_INFO_QUEUE})
     void onMessage(final Message message) {
 
-        //indexer le document
         IndexRequest request = new IndexRequest("games")
             .id(message.getMessageProperties().getHeaders().get("id").toString())
             .source(new String(message.getBody(), StandardCharsets.UTF_8), XContentType.JSON);
+//        indexSync(request);
+
+        indexAsync(request);
+    }
+
+    private void indexAsync(IndexRequest request) {
+        ActionListener<IndexResponse> listener = new ActionListener<IndexResponse>() {
+            @Override
+            public void onResponse(IndexResponse indexResponse) {
+                System.out.println(indexResponse.status());
+            }
+
+            @Override
+            public void onFailure(Exception e) {
+                throw new RuntimeException(e);
+            }
+        };
+        client.indexAsync(request, RequestOptions.DEFAULT, listener);
+    }
+
+    private void indexSync(IndexRequest request) {
         try {
-            IndexResponse response = client.index(request, RequestOptions.DEFAULT);
+            IndexResponse response = this.client.index(request, RequestOptions.DEFAULT);
+            System.out.println(response.status());
         } catch (ElasticsearchException e) {
             if (e.status() == RestStatus.CONFLICT)
                 throw e;
